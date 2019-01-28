fe0fbc3f2b248a3a47aa9f23160ea22c5fd0754c
Pancho <pinchepanchopincho@gmail.com>
2019-01-28 19:46:41 +0100

    fix

--- client/src/components/MainButton/MainButton.js
+++ client/src/components/MainButton/MainButton.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './MainButton.css';
 
-<<<<<<< HEAD
 const MainButton = (props) => {
   const {text} = props;
     return (
       <div className="main-button">
       {text}
-=======
-class MainButton extends Component {
-
-  render () {
-    return (
-      <div className="main-button">
-        {this.props.text}
->>>>>>> feat: signup v0
       </div>
     );
 }
