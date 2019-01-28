663cf81873b9dd2f26824fb0628dfffb645d552f
Pancho <pinchepanchopincho@gmail.com>
2019-01-28 21:13:12 +0100

    test

--- client/src/components/AddMember/AddMember.test.js
+++ client/src/components/AddMember/AddMember.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {render, fireEvent, cleanup} from 'react-testing-library';
-import AddMember from './AddMember';
+// import React from 'react';
+// import {render, fireEvent, cleanup} from 'react-testing-library';
+// import AddMember from './AddMember';
 
-afterEach(cleanup);
+// afterEach(cleanup);
 
 
 
