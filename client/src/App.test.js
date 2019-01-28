f7a915de75e9620603b2d3b460882663334b54e2
gaber85 <gabrielr85@gmail.com>
2019-01-28 20:28:53 +0100

    fix: refactoring and other issues

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,30 +1,21 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import App from './App';
-import NavBar from './components/NavBar';
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// import { shallow } from 'enzyme';
+// import App from './App';
+// import NavBar from './components/NavBar';
 
-
-
-// // it('renders without crashing', () => {
-// //   const div = document.createElement('div');
-// //   ReactDOM.render(<App />, div);
-// //   ReactDOM.unmountComponentAtNode(div);
-// // });
-
-
-describe('<App />', () => {
-  let wrapper ;
-  beforeEach(function (){ wrapper = shallow(<App />)});
-  it('should render App', () => {
-    expect(wrapper)
-  });
-  it('should render the navbar component', () => {
-    expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
-  });
-  // it('should render the navbar component', () => {
-  //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
-  // });
-  // it('should render the navbar component', () => {
-  //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
-  })
+// describe('<App />', () => {
+//   let wrapper ;
+//   beforeEach(() => { wrapper = shallow(<App />)});
+//   it('should render App', () => {
+//     expect(wrapper)
+//   });
+//   it('should render the navbar component', () => {
+//     expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+//   });
+//   // it('should render the navbar component', () => {
+//   //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+//   // });
+//   // it('should render the navbar component', () => {
+//   //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+//   })
--- client/src/components/SignUp/SignUp.test.js
+++ client/src/components/SignUp/SignUp.test.js
@@ -1,26 +1,25 @@
-// import React from 'react';
-// // import ReactDOM from 'react-dom';
-// import SignUp from './SignUp';
-// import {shallow} from 'enzyme';
+// // import React from 'react';
+// // // import ReactDOM from 'react-dom';
+// // import SignUp from './SignUp';
+// // import {shallow} from 'enzyme';
 
 
-  it('should render SignUp', () => {
-    const wrapper = shallow(<SignUp />)
-  })
+//   it('should render SignUp', () => {
+//     const wrapper = shallow(<SignUp />)
+//   })
 
-})
-describe('<SignUp />', () => {
-  let wrapper ;
-  beforeEach(function (){ wrapper = shallow(<SignUp />)});
-  it('should render SignUp', () => {
-    expect(wrapper)
-  });
-  it('should render a form', () => {
-    expect(wrapper.containsMatchingElement(<form />)).toEqual(true);
-  });
-  // it('should render the navbar component', () => {
-  //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
-  // });
-  // it('should render the navbar component', () => {
-  //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
-  })
+// describe('<SignUp />', () => {
+//   let wrapper ;
+//   beforeEach(function (){ wrapper = shallow(<SignUp />)});
+//   it('should render SignUp', () => {
+//     expect(wrapper)
+//   });
+//   it('should render a form', () => {
+//     expect(wrapper.containsMatchingElement(<form />)).toEqual(true);
+//   });
+//   // it('should render the navbar component', () => {
+//   //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+//   // });
+//   // it('should render the navbar component', () => {
+//   //   expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+//   })
