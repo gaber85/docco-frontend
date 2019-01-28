2dd1d40cc2fbebd1d2ecacedb32ad0e704cd0011
gaber85 <gabrielr85@gmail.com>
2019-01-28 13:02:14 +0100

    feat: add MainButton component

--- client/src/components/MainButton/MainButton.js
+++ client/src/components/MainButton/MainButton.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import './Plus-Button-Component.css';
+import './MainButton.css';
 
 class MainButton extends Component {
-  
+  render() {
+    return (
+      <div className="main-button">
+      {this.props.text}
+      </div>
+    );
+  }
 }
+
+export default MainButton;
