6ca2fe2ba4957e8896c98bfc595700e06cb16191
Pancho <pinchepanchopincho@gmail.com>
2019-01-28 15:54:02 +0100

    chore:add jest testing

---
+++ client/src/components/SignUp/SignUp.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import SignUp from './SignUp';
+import {shallow} from 'enzyme';
+
+describe('<SignUp />', () => {
+  it('should render SignUp', () => {
+    const wrapper = shallow(<SignUp />)
+  })
+
+})
+
