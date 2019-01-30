c31d797328ab7dc8f430c9f116f80973ece81530
gaber85 <gabrielr85@gmail.com>
2019-01-30 13:05:17 +0100

    feat: finalize ContractPage container and Contract Section

--- client/src/containers/ContractPage/ContractPage.js
+++ client/src/containers/ContractPage/ContractPage.js
@@ -3,27 +3,26 @@ import './ContractPage.css';
 import SearchBar from '../../components/SearchBar'
 import TeamSection from '../../components/TeamSection';
 import ContractSection from '../../components/ContractSection';
-import NavBar from '../../components/NavBar'
 // eslint-disable-next-line
 class ContractPage extends Component {
   render() {
     return (
       <div className="main-container">
-        <div className="container-middle">
-          <div className="team-section">
-            <TeamSection />
-          </div>
-          <div className="contract-display">
-            <div className="container-top">
-              <div className="title">Apple Contract</div>
-              <div className="search-bar-section"><SearchBar/></div>
-            </div>
-            <ContractSection />
-          </div>
-          <div className="sidebar-controls">
-            side bar
-          </div>
+        <div className="team-section">
+          <TeamSection />
         </div>
+        <div className="contract-display">
+          <div className="container-top">
+            <div className="title">Apple Contract</div>
+            <div className="search-bar-section"><SearchBar/></div>
+          </div>
+          <div className="contract">
+            <ContractSection className="contract-section" />
+            <div className="sidebar-controls">
+            side bar
+            </div>
+          </div>
+          </div>
       </div>
     )
   }
