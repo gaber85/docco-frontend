59969f4920fc2aefc663caeed20e7eb2cbcd0221
gaber85 <gabrielr85@gmail.com>
2019-02-06 00:17:49 +0100

    refactor: restyle with emotion, add main routing and guard

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,22 +1,73 @@
-import React from 'react';
-import './NavBar.css';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import Logo from './assets/Docco.png';
 import userImg from './assets/user-image.png';
 
-const NavBar = (props) => {
-    const logo = Logo;
-    const userPic = userImg;
-    const {img, name} = props
-    return (
-      <div className="nav-bar">
-        <img className="small-logo" alt="logo" src={logo} />
-        <div className="user">
-          <img className="user-image" src={img || userPic} alt="user-profile" />
-          <div className="user-name">{name}</div>
-          <div className="logout">Logout</div>
+const NavBar = props => {
+  const logo = Logo;
+  const userPic = userImg;
+  const { img, name } = props;
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: space-between;
+        background-color: #3498db;
+        height: 65px;
+        width: 100%;
+        align-items: center;
+      `}
+    >
+      <img
+        css={css`
+          height: 60px;
+          width: 80px;
+          cursor: pointer;
+        `}
+        alt="logo"
+        src={logo}
+      />
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          padding: 15px;
+        `}
+      >
+        <img
+          css={css`
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            margin-right: 10px;
+            background-color: white;
+          `}
+          src={img || userPic}
+          alt="user-profile"
+        />
+        <div
+          css={css`
+            color: white;
+            padding-right: 20px;
+          `}
+        >
+          {name}
+        </div>
+        <div
+          css={css`
+            color: white;
+            cursor: pointer;
+            &:hover {
+              color: black;
+            }
+          `}
+          className="logout"
+        >
+          Logout
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default NavBar;
